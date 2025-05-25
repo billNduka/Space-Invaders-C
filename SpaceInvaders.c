9abeaef42d5c819a8f44e3e4f15ea4f5ae19bb09
Emele winner <winnerumaemele2007@gmail.com>
2025-05-25 12:13:45 -0700

    Update SpaceInvaders.c
    initialized the ship

--- SpaceInvaders.c
+++ SpaceInvaders.c
@@ -1,26 +1,27 @@
+#define SDL_MAIN_HANDLED  
 #include <SDL2/SDL.h>
 #include <stdio.h>
-// first commit  by Emele Winner 
+// first commit  by Emele Winner orji-uma
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
     printf("\nStarting\n");
-    SDL_Window *window = SDL_CreateWindow("SDL Test",
+    SDL_Window* window = SDL_CreateWindow("SDL Test",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         640, 480, SDL_WINDOW_SHOWN);
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // red background
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 200); // red background
     SDL_RenderClear(renderer);
+    SDL_Rect ship_body = { 250, 440, 140, 100 };
+    SDL_Rect left_muzzle = { 270, 400, 10, 40 };
+    SDL_Rect right_muzzle = { 360, 400, 10, 40 };
+    SDL_SetRenderDrawColor(renderer, 255,255, 255, 255); 
+    SDL_RenderFillRect(renderer, &ship_body);
+    SDL_RenderFillRect(renderer, &left_muzzle);
+    SDL_RenderFillRect(renderer, &right_muzzle);
     SDL_RenderPresent(renderer);
-    SDL_Delay(2000);
-    SDL_RenderClear(renderer);
-    SDL_Rect rectangle = {200, 200, 200, 200};
-
-    SDL_SetRenderDrawColor(renderer, 25, 77, 100, 255);
-    SDL_RenderFillRect(renderer, &rectangle);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(3000); // keep window visible
+    SDL_Delay(30000); // keep window visible for 3 seconds 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
