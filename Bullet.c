250dc6692b30af432b046e8bea0a15dc524bf389
Emele winner <winnerumaemele2007@gmail.com>
2025-06-23 10:31:26 -0700

    Update Bullet.c

--- Bullet.c
+++ Bullet.c
@@ -17,7 +17,7 @@ void checkCollision(Bullet* bullet, Enemy* enemies, int numEnemies)
         if (enemies[i].active) 
         {
             SDL_Rect bulletRect = {bullet->x, bullet->y, 5, 10};
-            SDL_Rect enemyRect = {enemies[i].position[0], enemies[i].position[1], 25, 10};
+            SDL_Rect enemyRect = {enemies[i].position[0], enemies[i].position[1], 45, 10};
 
             if (SDL_HasIntersection(&bulletRect, &enemyRect)) 
             {
